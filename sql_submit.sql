a3c86e69262780a179163f2e07a82a2be8859227
ToshikiTomoyose <sf7fantazy7assaltsk@gmail.com>
2017-10-05 16:37:38 +0900

    sqlhazime

---
+++ sql_submit.sql
@@ -0,0 +1,35 @@
+﻿
+CREATE DATABASE likesubmit;
+USE likesubmit;
+CREATE TABLE item_category(
+	category_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	category_name varchar(256) NOT NULL
+	);
+	
+CREATE TABLE item(
+	item_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	item_name varchar(256) NOT NULL,
+	item_price int NOT NULL,
+	category_id int
+	);
+INSERT INTO item_category VALUES(1,'家具');
+INSERT INTO item_category VALUES(2,'食品');
+INSERT INTO item_category VALUES(3,'本');
+
+
+CREATE TABLE item(
+	item_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	item_name varchar(256) NOT NULL,
+	item_price int NOT NULL,
+	category_id int
+	);
+use likesubmit;
+
+INSERT INTO item VALUES(NULL,'堅牢な机',3000,1);
+INSERT INTO item VALUES(NULL,'生焼け肉',50,2);
+INSERT INTO item VALUES(NULL,'すっきりわかるJava入門',3000,3);
+INSERT INTO item VALUES(NULL,'おしゃれな椅子',2000,1);
+INSERT INTO item VALUES(NULL,'こんがり肉',500,2);
+INSERT INTO item VALUES(NULL,'書き方ドリルSQL',2500,3);
+INSERT INTO item VALUES(NULL,'ふわふわのベッド',30000,1);
+INSERT INTO item VALUES(NULL,'ミラノ風ドリア',300,2);
